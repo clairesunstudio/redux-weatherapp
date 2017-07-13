991316bafce6d79e9abfe7cca221ee275f5bbd08
Minghua Sun <clairesunstudio@gmail.com>
2017-07-13 14:40:36 -0400

    update state in event handler (bind this to call back func to in constructor to pass the component as the context for this)

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -4,21 +4,31 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props)
     this.state = { term: ''}
+    this.onInputChange = this.onInputChange.bind(this);
+    //binding the Component as the context of `this` to callback function and set the function to this.onInputChange
   }
 
   onInputChange(event){
     console.log(event.target.value)
+    this.setState({ term: event.target.value})
   }
 
   render() {
     return (
       <form className="input-group">
         <input
+          type="text"
+          placeholder="Search for a city to see weather forcast"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
         />
-        <span clssName="input-group-btn">
-          <button type="submit" className="btn btn-primary">Submit</button>
+        <span className="input-group-btn">
+          <button type="button" className="btn btn-secondary">Submit</button>
         </span>
       </form>
+
+
     )
   }
 }
