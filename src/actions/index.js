aef6b99e2537b5ee65c0b22ffd335b1e96589b36
Minghua Sun <clairesunstudio@gmail.com>
2017-07-13 15:19:07 -0400

    add action creator with API url and action.type as a const instead of a string for consistency

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY = '4f66c35905e554705ce85f0d5580fdbd'
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`  //template string (ES6 syntax)
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  }
+}
