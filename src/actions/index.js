f1a5b546de4f4ff733ac56b0377b9e6707f8f2f3
Minghua Sun <clairesunstudio@gmail.com>
2017-07-13 15:25:01 -0400

    install axios and use it to request url and return a promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,15 @@
+import axios from 'axios'
+
+
 const API_KEY = '4f66c35905e554705ce85f0d5580fdbd'
 const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`  //template string (ES6 syntax)
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url)
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
