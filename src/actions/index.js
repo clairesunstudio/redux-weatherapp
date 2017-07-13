58b9736ae4b06c56e7f0470df117eaf538411d4c
Minghua Sun <clairesunstudio@gmail.com>
2017-07-13 15:22:20 -0400

    formulate the full api url by attaching query parameters at the end

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ const API_KEY = '4f66c35905e554705ce85f0d5580fdbd'
 const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`  //template string (ES6 syntax)
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
   return {
     type: FETCH_WEATHER
   }
