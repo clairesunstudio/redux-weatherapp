978cd1ead234b90562f87689ce78954de3433f3c
Minghua Sun <clairesunstudio@gmail.com>
2017-07-14 13:31:24 -0400

    add avg reference lines to the charts and label

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,17 @@
+import _ from 'lodash'
 import React from 'react'
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
 //functional component
+const average = data => _.round(_.sum(data)/data.length);
 
 export default (props) => {
   return (
     <div>
-      <Sparklines data={props.data}>
+      <Sparklines svgHeight={100} svgWidth={200} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type='avg' />
       </Sparklines>
+      <div>{average(props.data)}{props.units}</div>
     </div>
   )
 }
